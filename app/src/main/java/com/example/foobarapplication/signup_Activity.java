58b58be8ffc70375f2097884ae9be22523d60218
shakedg1234 <shaked.gitta@gmail.com>
2024-02-08 15:40:04 +0200

    adding sign in page

--- app/src/main/java/com/example/foobarapplication/signup_Activity.java
+++ app/src/main/java/com/example/foobarapplication/signup_Activity.java
@@ -3,14 +3,12 @@ package com.example.foobarapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class signup_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnSignup= findViewById(requireViewById().getId());
+        setContentView(R.layout.activity_signup);
     }
 }
