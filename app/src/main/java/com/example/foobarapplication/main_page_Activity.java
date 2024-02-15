537e72c9785bc8ab1b8bb2da342310f5b0451acc
BarWanunu <barwanunu6@gmail.com>
2024-02-15 20:39:04 +0200

    working on main page

--- app/src/main/java/com/example/foobarapplication/main_page_Activity.java
+++ app/src/main/java/com/example/foobarapplication/main_page_Activity.java
@@ -1,14 +1,14 @@
-package com.example.foobarapplication;
+    package com.example.foobarapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+    import android.os.Bundle;
 
-public class main_page_Activity extends AppCompatActivity {
+    public class main_page_Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_page);
+        }
     }
-}
