4d3618311ac1867a61b25041c0739ebb9bd055ea
BarWanunu <barwanunu6@gmail.com>
2024-02-22 10:59:30 +0200

    working on posts and reading posts

---
+++ app/src/main/java/com/example/foobarapplication/readingPosts.java
@@ -0,0 +1,58 @@
+package com.example.foobarapplication;
+
+import static androidx.core.content.res.TypedArrayUtils.getResourceId;
+
+import android.content.Context;
+
+import com.example.foobarapplication.entities.Post;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class readingPosts {
+
+    public static void readingPosts(Context context) {
+        try {
+            InputStream inputStream = context.getResources().openRawResource(R.raw.Posts);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+            JSONArray jsonArray = new JSONArray("Posts");
+
+            List<Post> postList = GlobalVariables.postList;
+
+            for(int i=0; i<jsonArray.length(); i++) {
+                JSONObject jsonPost = jsonArray.getJSONObject(i);
+                int id = jsonPost.getInt("id");
+                String author = jsonPost.getString("author");
+                String content = jsonPost.getString("content");
+                String date = jsonPost.getString("date");
+                int likes = jsonPost.getInt("likes");
+                int picResourceId = getResourceId(context, jsonPost.getString("pic"));
+                int profilePicResourceId = getResourceId(context, jsonPost.getString("userPic"));
+                Post post = new Post(id, author, content, picResourceId, profilePicResourceId, likes, date);
+                postList.add(post);
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int getResourceId(Context context, String resourceName) {
+        return context.getResources().getIdentifier(resourceName, "drawable", context.getPackageName());
+    }
+}
